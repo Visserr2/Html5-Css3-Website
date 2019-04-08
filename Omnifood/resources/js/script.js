57588093b01f7d61b348fe660b29024c48a37ef7
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-04-08 19:50:47 +0200

    Added sticky menu
    Added sticky menu

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000')
+    var waypoint = new Waypoint({
+        element: $('.js--section-features'),
+        handler: function(direction) {
+            if(direction == "down"){
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+            }
+        }
     })
-})
+
+});
