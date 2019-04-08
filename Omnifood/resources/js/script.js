579d42b765ccb7c0f25c2b46c847ec1192cfc824
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-04-08 20:15:46 +0200

    Made buttons clickable
    Made buttons clickable

--- Omnifood/resources/js/script.js
+++ Omnifood/resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    /* For the sticky navigation */
     var waypoint = new Waypoint({
         element: $('.js--section-features'),
         handler: function(direction) {
@@ -11,4 +12,27 @@ $(document).ready(function() {
         }
     })
 
+    /* Scroll to sections */
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    })
+
+    $('.js--scroll-to-steps').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-steps').offset().top}, 1000);
+    })
+
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    })
+
+    $('.js--scroll-to-cities').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-cities').offset().top}, 1000);
+    })
+
+    $('.js--scroll-to-forms').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-forms').offset().top}, 1000);
+    })
+
+    /* animations on  Scroll*/
+    
 });
